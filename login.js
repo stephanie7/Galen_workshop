07fca42771d5120632c30ab268d8493f9f822493
stephanie7 <jjsun@thoughtworks.com>
2017-03-10 14:58:36 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
-driver.findElement(By.cssSelector("#cp_user_email")).sendKeys("admin@rea-group.com");
-driver.findElement(By.cssSelector("#cp_user_password")).sendKeys("please12");
+driver.findElement(By.cssSelector("#cp_user_email")).sendKeys("****");
+driver.findElement(By.cssSelector("#cp_user_password")).sendKeys("****");
 driver.findElement(By.cssSelector('.rui-button-brand')).click();
 
 

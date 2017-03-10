d7301a632bdf78c7923164d196d82f9a664109c3
Jingjing Sun <sun.jingjing@rea-group.com>
2017-03-10 11:17:31 +0800

    Galen_workshop

---
+++ login.js
@@ -0,0 +1,5 @@
+driver.findElement(By.cssSelector("#cp_user_email")).sendKeys("admin@rea-group.com");
+driver.findElement(By.cssSelector("#cp_user_password")).sendKeys("please12");
+driver.findElement(By.cssSelector('.rui-button-brand')).click();
+
+
